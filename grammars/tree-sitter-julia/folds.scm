32bfbdff1bd64eb637c5e24688c919f72afd87f6
Morgan Rodgers <morgan.joaquin@gmail.com>
2026-02-10 11:47:30 +0100

    `if` fold treatment

--- grammars/tree-sitter-julia/folds.scm
+++ grammars/tree-sitter-julia/folds.scm
@@ -1,25 +1,34 @@
 (
   [
+    (module_definition)
+    (struct_definition)
+    (macro_definition)
     (function_definition)
+    (for_statement)
+    (while_statement)
+    (let_statement)
+    (quote_statement)
+    (do_clause)
+    (compound_statement)
+    (string_literal)
+    (block_comment)
   ] @fold
-    (#set! fold.endAt endPosition)
 )
 
-; (
-;   [
-;     (module_definition)
-;     (struct_definition)
-;     (macro_definition)
-;     (function_definition)
-;     (if_statement)
-;     (try_statement)
-;     (for_statement)
-;     (while_statement)
-;     (let_statement)
-;     (quote_statement)
-;     (do_clause)
-;     (compound_statement)
-;     (string_literal)
-;     (block_comment)
-;   ] @fold
-; )
+; TODO want to treat these differently
+; (if_statement) @fold
+(try_statement) @fold
+
+
+((if_statement
+  alternative: [(elseif_clause) (else_clause)]) @fold
+  (#set! fold.endAt lastNamedChild.startPosition)
+  (#set! fold.adjustToEndOfPreviousRow true))
+
+((elseif_clause) @fold
+  (#set! fold.endAt endPosition))
+
+((else_clause) @fold
+  (#set! fold.endAt endPosition))
+
+(if_statement) @fold
